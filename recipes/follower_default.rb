c2d52a245e51c97fc8189ad9cc1893bf8f14d67f
andy-dufour <adufour@chef.io>
2016-05-27 14:59:44 -0400

    Updating to use databags

--- recipes/follower_default.rb
+++ recipes/follower_default.rb
@@ -7,6 +7,20 @@
 hostname = node['hostname']
 netname = node['tinc']['netname']
 
+all_data = data_bag('tinc_followers')
+node_data = Mash.new
+
+all_data.each do |n|
+  this_node = data_bag_item('tinc_followers', n)
+  if this_node['hostname'] == hostname
+    node_data = this_node
+  end
+end
+
+include_recipe "tinc-vpn::bootstrap" unless node_data['hostname']
+
+leader_node = data_bag_item('tinc', 'leader')
+
 # if on debian_family make sure that the apt repos are up to date
 if 'debian' == node['platform_family']
   include_recipe 'apt'
@@ -32,26 +46,42 @@ template "/etc/tinc/#{netname}/tinc.conf" do
   owner "root"
   group "root"
   mode "0644"
+  variables({:leader_name => leader_node['name'],
+    :node_name => node_data['name']})
 end
 
 # the hosts file for the network
-template "/etc/tinc/#{netname}/hosts/#{hostname}" do
+template "/etc/tinc/#{netname}/hosts/#{node_data['name']}" do
   source "hosts_tinc.erb"
   owner "root"
   group "root"
   mode "0644"
+  variables({:public_key => node_data['public_key'],
+    :ip => node_data['vpn_ip'],
+    :subnet => node_data['subnet']})
 end
 
-# create the private and public keys
-bash "create public private keys" do
-  user "root"
-  cwd "/etc/tinc/#{netname}/"
-  creates "/etc/tinc/#{netname}/rsa_key.priv"
-  code <<-EOH
-    STATUS=0
-    sudo tincd -n #{netname} -K4096 || STATUS=1
-    exit $STATUS
-  EOH
+if node_data['private_key']
+  file "/etc/tinc/#{netname}/rsa_key.priv" do
+    content node_data['private_key']
+  end
+  file "/etc/tinc/#{netname}/rsa_key.pub" do
+    content node_data['public_key']
+  end
+else
+  # create the private and public keys
+  bash "create public private keys" do
+    user "root"
+    cwd "/etc/tinc/#{netname}/"
+    creates "/etc/tinc/#{netname}/rsa_key.priv"
+    code <<-EOH
+      STATUS=0
+      sudo tincd -n #{netname} -K4096 || STATUS=1
+      exit $STATUS
+    EOH
+  end
+
+  #TODO: Save keys to node databag
 end
 
 # create the tinc-up script
@@ -60,6 +90,7 @@ template "/etc/tinc/#{netname}/tinc-up" do
   owner "root"
   group "root"
   mode "0755"
+  variables ({:ip => node_data['vpn_ip']})
 end
 
 # create the tinc-down script
